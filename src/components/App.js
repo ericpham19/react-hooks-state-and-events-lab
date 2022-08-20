001b91261a44f45352f608e663330cae4dbc3fee
Duy Pham <ericpham19@outlook.com>
2022-08-20 15:31:25 +1000

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,26 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [background, setBackground] = useState (false)
+
+    
+
+    function handleClick (){
+      setBackground((background) => !background)
+    }  
+  
+  
+ 
+  
 
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (background ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick = {handleClick}>{background ? "dark" : "light"}</button> 
       </header>
       <ShoppingList items={items} />
     </div>
